44856b34dfaed40284702af902b44005cb863b37
philpar <90182831+philpar21@users.noreply.github.com>
2022-05-19 11:55:03 +0200

    Added first js

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const element = document.getElementById('createtodo');
+
+function addNewTodo(){
+    window.open('details.html', "_self");
+}
+
+element.addEventListener('click', addNewTodo);
+
+const elementCompleted = document.getElementById('taskcomplete');
+
+function markCompleted(){
+    elementCompleted.style.backgroundColor = 'red';
+    elementCompleted.style.color = 'white';
+    elementCompleted.innerHTML = 'Complete';
+}
+
+elementCompleted.addEventListener('click', markCompleted);
+
+const sorting = document.querySelector('.filters-drpd');
+
+function sortElements(){
+    sorting.innerHTML = '↑';
+}
+
+sorting.addEventListener('click', sortElements);
+
